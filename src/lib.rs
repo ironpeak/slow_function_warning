4a70c00c67cf0c57f16ee582bda5848cc6edb6f3
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2024-10-31 16:58:08 +0000

    bevy feature

--- src/lib.rs
+++ src/lib.rs
@@ -6,63 +6,39 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::{quote, ToTokens};
 use syn::{
-    parse::{Parse, ParseStream},
-    parse_macro_input,
-    punctuated::Punctuated,
-    token::Semi,
-    Expr, Item, Lit, LitStr, Result, Stmt, Token,
+    parse_macro_input, punctuated::Punctuated, spanned::Spanned, token::Semi, Expr, Item, ItemFn,
+    Lit, LitStr, Result, Stmt, Token,
 };
 
-struct Args {
-    time: Duration,
-    stmt: Stmt,
-}
-
-impl Parse for Args {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let mut punctuated: Vec<Expr> =
-            Punctuated::<Expr, Token![,]>::parse_separated_nonempty(input)?
-                .into_iter()
-                .collect();
-        if punctuated.len() == 0 {
-            return Err(syn::Error::new(input.span(), "Expected a numeric literal"));
-        }
-        let time = match &punctuated[0] {
-            Expr::Lit(expr_lit) => match &expr_lit.lit {
-                syn::Lit::Int(literal) => match literal.suffix() {
-                    "ns" => Duration::from_nanos(literal.base10_parse::<u64>()?),
-                    "ms" => Duration::from_millis(literal.base10_parse::<u64>()?),
-                    "s" => Duration::from_secs(literal.base10_parse::<u64>()?),
-                    "m" => Duration::from_secs(literal.base10_parse::<u64>()? * 60),
-                    "h" => Duration::from_secs(literal.base10_parse::<u64>()? * 60 * 60),
-                    "d" => Duration::from_secs(literal.base10_parse::<u64>()? * 60 * 60 * 24),
-                    "" => Duration::from_millis(literal.base10_parse::<u64>()?),
-                    suffix => {
-                        return Err(syn::Error::new(
-                            input.span(),
-                            format!("Unexpected a numeric literal suffix {}", suffix),
-                        ))
-                    }
-                },
-                _ => {
-                    return Err(syn::Error::new(input.span(), "Expected a numeric literal"));
+fn parse_time(expr: &Expr) -> Result<Duration> {
+    match expr {
+        Expr::Lit(expr_lit) => match &expr_lit.lit {
+            syn::Lit::Int(literal) => match literal.suffix() {
+                "ns" => Ok(Duration::from_nanos(literal.base10_parse::<u64>()?)),
+                "ms" => Ok(Duration::from_millis(literal.base10_parse::<u64>()?)),
+                "s" => Ok(Duration::from_secs(literal.base10_parse::<u64>()?)),
+                "m" => Ok(Duration::from_secs(literal.base10_parse::<u64>()? * 60)),
+                "h" => Ok(Duration::from_secs(
+                    literal.base10_parse::<u64>()? * 60 * 60,
+                )),
+                "d" => Ok(Duration::from_secs(
+                    literal.base10_parse::<u64>()? * 60 * 60 * 24,
+                )),
+                "" => Ok(Duration::from_millis(literal.base10_parse::<u64>()?)),
+                suffix => {
+                    return Err(syn::Error::new(
+                        expr.span(),
+                        format!("Unexpected a numeric literal suffix {}", suffix),
+                    ))
                 }
             },
             _ => {
-                return Err(syn::Error::new(input.span(), "Expected a numeric literal"));
+                return Err(syn::Error::new(expr.span(), "Expected a numeric literal"));
             }
-        };
-        let stmt = if punctuated.len() >= 2 {
-            let expr: Expr = syn::parse(punctuated.pop().unwrap().to_token_stream().into())?;
-            Stmt::Expr(expr, Some(Semi::default()))
-        } else {
-            syn::parse(
-                quote! {
-                println!("Warning: {module}::{function}: ran for {millis}ms"); }
-                .into(),
-            )?
-        };
-        Ok(Args { time, stmt })
+        },
+        _ => {
+            return Err(syn::Error::new(expr.span(), "Expected a numeric literal"));
+        }
     }
 }
 
@@ -84,13 +60,81 @@ pub fn release_slow_function_warning(args: TokenStream, input: TokenStream) -> T
 
 #[proc_macro_attribute]
 pub fn slow_function_warning(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args: Args = parse_macro_input!(args as Args);
-    let Item::Fn(mut function) = syn::parse(input.clone()).unwrap() else {
+    let args: Punctuated<Expr, Token![,]> =
+        parse_macro_input!(args with Punctuated::<Expr, Token![,]>::parse_separated_nonempty)
+            .into_iter()
+            .collect();
+    let Item::Fn(function) = syn::parse(input).unwrap() else {
         panic!("slow_function_warning can only be used on functions");
     };
+    let time = if let Some(time_expr) = args.get(0) {
+        parse_time(time_expr).unwrap()
+    } else {
+        panic!("slow_function_warning missing time argument");
+    };
+    let stmt = if let Some(stmt) = args.get(1) {
+        Stmt::Expr(stmt.clone(), Some(Semi::default()))
+    } else {
+        syn::parse(
+            quote! {
+                println!("Warning: {module}::{function}: ran for {millis}ms");
+            }
+            .into(),
+        )
+        .unwrap()
+    };
+    slow_function_warning_common(time, stmt, function)
+}
 
-    let stmt = args.stmt;
-    let nano_seconds = args.time.as_nanos();
+#[proc_macro_attribute]
+#[cfg(feature = "bevy")]
+pub fn debug_slow_system_warning(args: TokenStream, input: TokenStream) -> TokenStream {
+    if !cfg!(debug_assertions) {
+        return input;
+    }
+    slow_system_warning(args, input)
+}
+
+#[proc_macro_attribute]
+#[cfg(feature = "bevy")]
+pub fn release_slow_system_warning(args: TokenStream, input: TokenStream) -> TokenStream {
+    if cfg!(debug_assertions) {
+        return input;
+    }
+    slow_system_warning(args, input)
+}
+
+#[proc_macro_attribute]
+#[cfg(feature = "bevy")]
+pub fn slow_system_warning(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args: Punctuated<Expr, Token![,]> =
+        parse_macro_input!(args with Punctuated::<Expr, Token![,]>::parse_separated_nonempty)
+            .into_iter()
+            .collect();
+    let Item::Fn(function) = syn::parse(input).unwrap() else {
+        panic!("slow_system_warning can only be used on functions");
+    };
+    let time = if let Some(time_expr) = args.get(0) {
+        parse_time(time_expr).unwrap()
+    } else {
+        panic!("slow_system_warning missing time argument");
+    };
+    let stmt = if let Some(stmt) = args.get(1) {
+        Stmt::Expr(stmt.clone(), Some(Semi::default()))
+    } else {
+        syn::parse(
+            quote! {
+                warn!("{function}: ran for {millis}ms");
+            }
+            .into(),
+        )
+        .unwrap()
+    };
+    slow_function_warning_common(time, stmt, function)
+}
+
+fn slow_function_warning_common(time: Duration, stmt: Stmt, mut function: ItemFn) -> TokenStream {
+    let nano_seconds = time.as_nanos();
     let function_name = Lit::Str(LitStr::new(
         &function.sig.ident.to_string(),
         Span::call_site(),
